790134c12ae718515b9e52d23ba96d9443279fc8
Emmanuel Darkwa <emmanueldarkwaampadu@gmail.com>
2023-07-23 22:09:39 +0000

    my reviews

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'alex nolla',
+    job: 'web developer',
+    image: 'https://media.licdn.com/dms/image/D4E35AQEOcOi6nkEWng/profile-framedphoto-shrink_200_200/0/1673208827698?e=1690743600&v=beta&t=8_bcyjvBp3FxeSo5bzQsX201F17CQiuWh__WmhGLXHI',
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'margerette anim',
+    job: 'web designer',
+    image: 'https://course-api.com/images/people/person-2.jpeg',
+    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'edwin ampadu',
+    job: 'intern',
+    image: 'https://media.licdn.com/dms/image/C4D03AQHbl6dLCBShsw/profile-displayphoto-shrink_200_200/0/1628715873342?e=1695859200&v=beta&t=XI-u-PUyQ75_CDeG018wKR1hCL9Qndz3828BAVRN6ck',
+    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'emmanuel darkwa ampadu',
+    job: 'the boss',
+    image: 'https://media.licdn.com/dms/image/D4E03AQHrK4L24wI_2w/profile-displayphoto-shrink_200_200/0/1666534632312?e=1695859200&v=beta&t=pLgnBOu1yQsRO2J2Gjg9HWohelGtTShHZH2_i9Uwsvo',
+    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+];
+
+export default reviews;
